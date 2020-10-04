fbb282a4b51ab93bd12e37c48f0c810a26ba8b03
Bernardo <programming@bernardomg.com>
2020-10-04 16:42:19 +0200

    Configured auditing and HTTP trace endpoints

--- src/main/java/com/bernardomg/example/boot/actuator/config/ServiceConfig.java
+++ src/main/java/com/bernardomg/example/boot/actuator/config/ServiceConfig.java
@@ -1,6 +1,11 @@
 
 package com.bernardomg.example.boot.actuator.config;
 
+import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -8,6 +13,16 @@ public class ServiceConfig {
 
     public ServiceConfig() {
         super();
+    }
+
+    @Bean("auditEventRepository")
+    public AuditEventRepository getAuditEventRepository() {
+        return new InMemoryAuditEventRepository();
+    }
+
+    @Bean("httpTraceRepository ")
+    public HttpTraceRepository getHttpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 
 }
